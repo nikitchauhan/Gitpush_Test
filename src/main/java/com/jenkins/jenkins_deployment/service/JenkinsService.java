67ff9b26488e02e427d2b4957e8e3a4f689f5b51
Nikit <nikitchauhan527@gmail.com>
2026-08-12 17:58:04 +0530

    Third push

--- src/main/java/com/jenkins/jenkins_deployment/service/JenkinsService.java
+++ src/main/java/com/jenkins/jenkins_deployment/service/JenkinsService.java
@@ -12,6 +12,6 @@ public class JenkinsService {
 
     public ResponseEntity<?> test()
     {
-        return ResponseEntity.ok("Working JENKINS Test");
+        return ResponseEntity.ok("Working JENKINS Test Properly");
     }
 }
